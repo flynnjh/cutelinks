0846a1dcb26197610c939c3d1f1e7f6c8766ab04
flynn <flynn@dead.place>
2022-08-04 19:25:05 +0100

    updated frontend with styling improvments and deletion support :^)

--- pages/delete.js
+++ pages/delete.js
@@ -3,27 +3,19 @@ import Link from "next/link";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 
-export default function Home() {
-  const [url, setURL] = useState();
+export default function DeletePage() {
   const [slug, setSlug] = useState();
+  const [pin, setPin] = useState();
   const [info, setInfo] = useState();
   const [errorMessage, setErrorMessage] = useState();
 
-
   const handleSubmit = async () => {
     axios
-      .post(`/api/create/`, {
-        url: url,
-        slug: slug,
+      .post(`/api/delete/${slug}`, {
+        pin: pin,
       })
       .then(function (response) {
-
-        const shortLinkInfo = {
-          slug: response.data.slug,
-          pinReminder: response.data.pinReminder
-        }
-
-        setInfo(shortLinkInfo);
+        setInfo(response.data.message);
         setSlug("");
       })
       .catch(function (error) {
@@ -40,7 +32,7 @@ export default function Home() {
     <div className="flex flex-col h-screen justify-center place-items-center bg-slate-900/60">
       <div className="w-full max-w-lg max-h-lg">
         <form className="bg-slate-700/20 ring-1 ring-inset ring-white/10 shadow-2xl rounded-2xl px-8 pt-6 pb-8 mb-4">
-        <h1 className="flex justify-center text-4xl font-semibold text-slate-200/95 py-4">Cutelinks</h1>
+        <h1 className="flex justify-center text-4xl font-semibold text-slate-200/95 py-4">Delete your Cutelink :(</h1>
         <div className="flex flex-row text-lg justify-center font-bold text-sky-400/95 pb-4 gap-6 w-auto">
           <Link href="/"><a className="hover:underline">Create</a></Link>
           <Link href="/delete"><a className="hover:underline">Delete</a></Link>
@@ -48,43 +40,38 @@ export default function Home() {
           <div className="mb-4 mt-2">
             <input
               className="shadow rounded w-full py-2 px-3 leading-tight focus:outline-none focus:shadow-outline bg-base-100 ring-1 ring-inset ring-white/40"
-              id="url"
+              id="slug"
               type="text"
-              placeholder="Paste your long URL"
-              onChange={(e) => setURL(e.target.value)}
+              placeholder="Slug for your short link (e.g. hv)..."
+              onChange={(e) => setSlug(e.target.value)}
             />
           </div>
           <div className="mb-6">
             <input
               className="shadow appearance-none rounded w-full py-2 px-3 mb-3 leading-tight focus:outline-none focus:shadow-outline bg-base-100 ring-1 ring-inset ring-white/40"
-              id="slug"
+              id="pin"
               type="text"
-              placeholder="Enter your slug (optional)"
-              onChange={(e) => setSlug(e.target.value)}
+              placeholder="Pin to your Slug (e.g. '30')..."
+              onChange={(e) => setPin(e.target.value)}
             />
           </div>
           <div className="flex items-center justify-between">
             <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full ring-1 ring-inset ring-blue-500/50"
+              className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full ring-1 ring-inset ring-red-500/50"
               type="button"
               onClick={handleSubmit}
             >
-              Create!
+              Delete! :O
             </button>
           </div>
           {info ? (
             <div className="flex flex-col justify-center pt-6 font-semibold">
-              <Link href={info.slug}><a>
                 <h1 className="text-sky-400 hover:underline">
-                  {info.slug} created successfully!
-                </h1>
-              </a>
-              </Link>
-                <h1 className="py-4">
-                  Your pin for this slug is '{info.pinReminder}'. Remember it if you want to delete your slug manually!
+                  {info}
                 </h1>
             </div>
           ) : null}
+
           {errorMessage ? <h1>{errorMessage}</h1> : null}
         </form>
       </div>
