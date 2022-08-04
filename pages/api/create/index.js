781f0e3ea203f5242e31440eec391dcd753e0540
flynn <flynn@dead.place>
2022-08-04 19:25:39 +0100

    updated backend to allow for manual shortlink deletion :^)

--- pages/api/create/index.js
+++ pages/api/create/index.js
@@ -1,21 +1,39 @@
 import { prisma } from "../../../utils/prisma";
 import moment from "moment";
+import bcrypt from "bcrypt";
 
 export default async (req, res) => {
+  const generatePin = async () => {
+    const numbers = "0123456789";
+    let id = "";
+  
+    while (id.length < 2) {
+      id += numbers[Math.floor(Math.random() * numbers.length)];
+    }
+  
+    const salt = await bcrypt.genSalt(10);
+    const hashedPin = await bcrypt.hash(id,salt);
+    return [hashedPin, id];
+  };
+
   const createShortLink = async (url, slug) => {
+    const pin = await generatePin();
     const data = await prisma.shortLink.create({
       data: {
         url: url,
         slug: slug,
         ttl: ttl,
+        hashedPin: pin[0]
       },
     });
 
     return res.status(200).json({
       url: `${data.url}`,
       slug: `${data.slug}`,
+      pinReminder: `${pin[1]}`,
     });
   };
+
 
   if (!req.body.url) {
     return res.status(400).json({ message: "url is a required field :(" });
@@ -44,6 +62,7 @@ export default async (req, res) => {
       return res
         .status(400)
         .json({ message: `a slug called ${req.body.slug} already exists :(` });
+        
     }
     createShortLink(req.body.url, req.body.slug);
   }
--- pages/api/delete/[slug].js
+++ pages/api/delete/[slug].js
@@ -1,8 +1,10 @@
 import { prisma } from "../../../utils/prisma";
+import bcrypt from "bcrypt";
 
 export default async (req, res) => {
-  // TODO: Replace Search Slug with try catch delete https://www.prisma.io/docs/reference/api-reference/prisma-client-reference#delete
-  const searchSlug = await prisma.shortLink.findUnique({
+
+  // gets the slug's hashedPin
+  const searchSlug = await prisma.shortLink.findUnique({ 
     where: {
         slug: req.query.slug
     }
@@ -12,13 +14,28 @@ export default async (req, res) => {
     return res.status(400).json({ message: `a slug called ${req.query.slug} does not exist :(` });
   }
 
-  const deletedSlug = await prisma.shortLink.delete({
-    where: {
-        slug: req.query.slug
-    }
-  })
+  if (!req.body.pin) {
+    return res.status(400).json({ message: `you must provide the currect pin to delete your slug >:()` });
+  }
 
-  return res.status(200).json({
-    message: `${deletedSlug.slug} has been deleted. farewell little guy.`,
+  const validPin = bcrypt.compare(req.body.pin, searchSlug.hashedPin, async (err, data) => {
+    if (err) {
+      return err
+    }
+  
+    if (data) {
+      const deletedSlug = await prisma.shortLink.delete({
+        where: {
+            slug: req.query.slug
+        }
+      })
+    
+      return res.status(200).json({
+        message: `${deletedSlug.slug} has been deleted. farewell little guy.`,
+      });
+    } else {
+      return res.status(401).json({ message: "incorrect pin >:(" })
+    }
   });
+
 };
