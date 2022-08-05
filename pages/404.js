e9c78a6f58e8a0d33b74914f08a4f1377da127c3
flynn <flynn@dead.place>
2022-08-05 02:14:04 +0100

    minor improvements

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,11 @@
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
 
 export default function FourOhFour() {
   return <>
+  <Head>
+    <title>404 - slug not found :(</title>
+  </Head>
     <div className='flex flex-col justify-center place-items-center text-xl p-9 h-screen bg-slate-900/60'>
         <h1>slug not found :(</h1>
         <Link href="/">
